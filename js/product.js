54d1082f35f6c0c6e9c91ac53a44a14bd19f09a1
arnkeenan-gif <arnkeenan@gmail.com>
2026-04-18 15:49:06 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -90,7 +90,7 @@ function renderProduct(product) {
           <circle cx="9" cy="21" r="1"/><circle cx="20" cy="21" r="1"/>
           <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"/>
         </svg>
-        Add to cart
+        Tilføj til kurv
       </button>
 
       <div class="certifications">${certsHTML}</div>
