938bc1d2bf6ddc7c723ebe7d23e58f5d075915b1
arnkeenan-gif <arnkeenan@gmail.com>
2026-04-18 18:32:32 +0200

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
         {
           shipping_rate_data: {
             type: 'fixed_amount',
-            fixed_amount: { amount: 250, currency: 'dkk' },
+            fixed_amount: { amount: 4900, currency: 'dkk' },
             display_name: 'GLS – Pakkeshop',
             delivery_estimate: {
               minimum: { unit: 'business_day', value: 1 },
